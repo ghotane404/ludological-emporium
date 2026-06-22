17b68c8a2c309fc457f0003c7964ecd720efb8a2
ghotane404 <62863408+ghotane404@users.noreply.github.com>
2026-06-22 14:39:45 -0700

    CategoriesController endpoints and admin restrictions implemented

--- capstone-api-starter/src/main/java/org/yearup/service/CategoryService.java
+++ capstone-api-starter/src/main/java/org/yearup/service/CategoryService.java
@@ -1,6 +1,8 @@
 package org.yearup.service;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 import org.yearup.models.Category;
 import org.yearup.repository.CategoryRepository;
 
@@ -16,25 +18,36 @@ public class CategoryService {
 
 	public List<Category> getAllCategories() {
 		// get all categories
-		return null;
+		return categoryRepository.findAll();
 	}
 
 	public Category getById(int categoryId) {
 		// get category by id
-		return null;
+		return categoryRepository.findById(categoryId)
+				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found"));
 	}
 
 	public Category create(Category category) {
 		// create a new category
-		return null;
+		return categoryRepository.save(category);
 	}
 
 	public Category update(int categoryId, Category category) {
 		// update category and return the updated category
-		return null;
+		Category updateCategory = categoryRepository.findById(categoryId)
+				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found"));
+
+		updateCategory.setName(category.getName());
+		updateCategory.setDescription(category.getDescription());
+
+		return categoryRepository.save(updateCategory);
 	}
 
 	public void delete(int categoryId) {
 		// delete category
+		Category category = categoryRepository.findById(categoryId)
+				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found"));
+
+		categoryRepository.delete(category);
 	}
 }
