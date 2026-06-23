a643273592944eb7eb126944a9ef176a0e375c89
Ghotane <62863408+ghotane404@users.noreply.github.com>
2026-06-22 21:30:31 -0700

    Revert "1 feature implement categoriescontroller"

--- capstone-api-starter/src/test/java/org/yearup/repository/ProductRepositoryTest.java
+++ capstone-api-starter/src/test/java/org/yearup/repository/ProductRepositoryTest.java
@@ -6,46 +6,27 @@ import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 import org.springframework.test.context.jdbc.Sql;
 import org.yearup.models.Product;
 
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @DataJpaTest
 @Sql(scripts = "classpath:test-insert-data.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
-class ProductRepositoryTest {
-	@Autowired
-	private ProductRepository productRepository;
+class ProductRepositoryTest
+{
+    @Autowired
+    private ProductRepository productRepository;
 
-	@Test
-	void findAll_shouldReturnAllProductsFromDatabase() {
-		// act
-		List<Product> products = productRepository.findAll();
+    @Test
+    public void getById_shouldReturn_theCorrectProduct()
+    {
+        // arrange
+        int productId = 1;
 
-		// assert
-		assertEquals(8, products.size());
-	}
+        // act
+        Product actual = productRepository.findById(productId).orElse(null);
 
-	@Test
-	void findByCategoryId_shouldReturnProductsInCategory() {
-		// act
-		List<Product> products = productRepository.findByCategoryId(1);
-
-		// assert
-		assertEquals(3, products.size());
-	}
-
-	@Test
-	public void getById_shouldReturn_theCorrectProduct() {
-		// arrange
-		int productId = 1;
-
-		// act
-		Product actual = productRepository.findById(productId).orElse(null);
-
-		// assert
-		assertNotNull(actual, "Because product 1 should exist in the test database.");
-		assertEquals("The Legend of Zelda: Breath of the Wild", actual.getName());
-		assertEquals(59.99, actual.getPrice(), 0.001, "Because product 1 is Zelda in the test database.");
-	}
+        // assert
+        assertNotNull(actual, "Because product 1 should exist in the test database.");
+        assertEquals(499.99, actual.getPrice(), 0.001, "Because I tried to get product 1 from the database.");
+    }
 }
